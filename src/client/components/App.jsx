e0d32343fc06679b8d5b0b24c824e4b337a1bcda
eternalee <one.eternity@gmail.com>
2019-01-30 01:40:53 -0800

    calculation and time calculations

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
 
   const [startDate, setStartDate] = useState('');
-  const [numberDays, setNumberDays] = useState(0);
+  const [numberOfDays, setNumberOfDays] = useState(0);
   const [totalCost, setTotalCost] = useState('');
 
-  useEffect(() => { console.log(startDate, numberDays, totalCost) });
+  useEffect(() => { console.log(startDate, numberOfDays, totalCost) });
   return (
     <div>
       <h1>Bob's Banana Budget</h1>
@@ -19,13 +19,13 @@ export default function App() {
           headers: { 'Content-Type': 'application/json; charset=utf-8' },
           body: JSON.stringify({
             'startDate': startDate,
-            'numberDays': numberDays
+            'numberOfDays': numberOfDays
           })
         })
           .then((res) => res.json())
           .then((res) => {
-            res.msg === 'Calculation success' ? setTotalCost(res.budget) : alert('Invalid input');
-            setStartDate(''); setNumberDays(0);
+            res.msg === 'Calculation success' ? setTotalCost(res.totalCost) : alert('Invalid input');
+            setStartDate(''); setNumberOfDays(0);
           });
 
       }}>
@@ -33,14 +33,11 @@ export default function App() {
           <span>Beginning Date</span><input type='date' min='2019-01-01' value={startDate} onChange={e => setStartDate(e.target.value)} />
         </div>
         <div>
-          <span>Number of Days</span><input type='text' placeholder='number of days*' value={numberDays} onChange={e => setNumberDays(e.target.value)} />
-          <p>*Including the beginning date. Bannas are only purchased on weekdays.</p>
+          <span>Number of Days</span><input type='text' placeholder='number of days*' value={numberOfDays} onChange={e => setNumberOfDays(e.target.value)} />
         </div>
-
-
         <button id='submit-button' value="Submit">submit</button>
       </form>
-      <div>Total Cost: ${totalCost}</div>
+      <div><h2>Total Cost: ${totalCost}</h2></div>
     </div>
   );
 }
--- src/server/Controller.js
+++ src/server/Controller.js
@@ -1,10 +1,53 @@
 module.exports = {
   calculate(req, res) {
-    console.log('req', req.body)
-    console.log('req received by Controller');
+    /* bananas are only purchased on weekdays 
+    Month:
+    Day   1-7 = .05
+    Day  8-14 = .10
+    Day 15-21 = .15
+    Day 22-28 = .20
+    Day 29-31 = .25
+    */
+    let num = parseInt(req.body.numberOfDays)
+    let d = new Date(req.body.startDate);
+
+    //calculate endDate as "num" days from the startDate
+    let endDate = new Date(req.body.startDate);
+    endDate.setDate(endDate.getDate() + num)
+
+    //create an array of all weekdates in specified time period
+    let dateArr = [];
+    for (let date = d; date < endDate; date.setUTCDate(date.getUTCDate() + 1)) {
+      if (date.getDay() != 5 && date.getDay() != 6) {
+        dateArr.push(new Date(date));
+      }
+    }
+
+    //create an array representing only days of the month
+    let numbers = [];
+    dateArr.forEach(el => numbers.push(el.getUTCDate()));
+
+    //calculate the total cost
+    let totalCost = numbers.reduce((acc, cv) => {
+      if (cv > 0 && cv < 8) {
+        acc += .05
+      } else if (cv >= 8 && cv < 15) {
+        acc += .10
+      } else if (cv >= 15 && cv < 22) {
+        acc += .15
+      } else if (cv >= 22 && cv < 29) {
+        acc += .20
+      } else {
+        acc += .25
+      }
+      return acc;
+    }, 0)
+
+    dateArr = [];
+
     res.header(200).send({
       msg: 'Calculation success',
-      budget: 99
+      totalCost: totalCost
     })
   }
 }
